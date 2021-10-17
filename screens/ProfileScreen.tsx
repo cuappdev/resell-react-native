b077e32c9fce78a03f03edf47b1a7e5e58da1a58
Sergio Diaz <spd75@cornell.edu>
2021-10-17 11:06:15 -0400

    Setup for profile screen

---
+++ screens/ProfileScreen.tsx
@@ -0,0 +1,74 @@
+import { processFontFamily } from 'expo-font';
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+import Icon from '../assets/svg-icons/settings.svg';
+import { Text, View } from '../components/Themed';
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.container}>
+        <View style={styles.upperContainer}>
+            <View style={styles.profileBubbleContainer}>
+              <View style={styles.profileBubble} />
+            </View>
+            <View style={styles.profileTextContainer}>
+                <Text style={styles.profileNameText}>ravina</Text>
+                <Text style={styles.profileBioText}>selling cool stuff!</Text>
+            </View>
+            <View>
+                <Icon width={26} height={26} />
+            </View>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    paddingVertical: 18,
+    paddingHorizontal: 24
+  },
+  upperContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'stretch'
+  },
+  profileBubbleContainer: {
+    flexGrow: 1
+  },
+  profileBubble: {
+    width: 89,
+    height: 89,
+    borderRadius: 50,
+    backgroundColor: '#e0e0e0',
+  },
+  profileTextContainer: {
+    display: 'flex',
+    flexGrow: 4,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  profileNameText: {
+    fontFamily: 'Roboto-Bold',
+    paddingBottom: 5,
+    fontSize: 23
+  },
+  profileBioText: {
+    fontFamily: 'Roboto-Regular',
+    fontSize: 17
+  }
+});
