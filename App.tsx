b1b3220a9a81a43401fc0a5c7a528c611428d185
Sergio Diaz <spd75@cornell.edu>
2021-10-17 11:19:08 -0400

    Revert index to original state

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
+import * as Font from "expo-font";
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import * as Font from "expo-font";
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
