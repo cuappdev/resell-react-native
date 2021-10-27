af21bfdde9ac1d3683150ed828628bf9ac3523f9
Sergio Diaz <spd75@cornell.edu>
2021-10-27 18:52:27 -0400

    Progress on changing format

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,23 +1,34 @@
 import * as React from 'react';
-import Icon from '../assets/svg-icons/settings.svg';
-import { StyleSheet } from 'react-native';
+import PlusIcon from '../assets/svg-icons/plus-circle.svg';
+import SettingsIcon from '../assets/svg-icons/settings.svg';
+import SearchIcon from '../assets/svg-icons/search.svg';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, View } from '../components/Themed';
+
+/* Constants for in TSX inherited properties */
+const pressedOpacity = 0.3;
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
         <View style={styles.upperContainer}>
-            <View style={styles.profileBubbleContainer}>
-              <View style={styles.profileBubble} />
-            </View>
+              <TouchableOpacity activeOpacity={pressedOpacity}>
+                <SettingsIcon />
+              </TouchableOpacity>
             <View style={styles.profileTextContainer}>
-                <Text style={styles.profileNameText}>ravina</Text>
-                <Text style={styles.profileBioText}>selling cool stuff!</Text>
+                <View style={styles.profileBubble} />
+                <Text style={styles.profileNameText}>Sergio Pablo Diaz</Text>
+                <Text style={styles.profileBioText}>Junior in the college of engineering. Selling meth, pot, and crack cocaine. This some more sample text to see whether or not the padding disappears.</Text>
             </View>
             <View>
-                <Icon width={26} height={26} />
+              <TouchableOpacity activeOpacity={pressedOpacity}>
+                <SearchIcon />
+                </TouchableOpacity>
             </View>
         </View>
+        <TouchableOpacity style={styles.plusButton}>
+                <PlusIcon width={36} height={36} />
+        </TouchableOpacity>
     </View>
   );
 }
@@ -48,16 +59,35 @@ const styles = StyleSheet.create({
     flexGrow: 4,
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'flex-start'
+    alignItems: 'center'
   },
   profileNameText: {
     fontFamily: 'Roboto-Bold',
-    fontSize: 23,
-    paddingBottom: 5
+    fontSize: 20,
+    paddingTop: 12,
+    paddingBottom: 9
   },
   profileBioText: {
     fontFamily: 'Roboto-Regular',
-    fontSize: 17
+    fontSize: 16,
+    maxWidth: '88%',
+    textAlign: 'center'
+  },
+  smallIcon: {
+    height: 24,
+    width: 24
+  },
+  plusButton: {
+    position: 'absolute',
+    backgroundColor: '#000',
+    float: 'right',
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 5
+  },
+  plusIcon: {
+    height: 36,
+    width: 36
   }
 });
