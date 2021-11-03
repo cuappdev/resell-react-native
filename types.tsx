736a3c150e09d329bd466c77a5ddfa0a77a236d1
Junyu Wang <jw838@cornell.edu>
2021-11-03 18:59:51 -0400

    tab and initial layout

--- types.tsx
+++ types.tsx
@@ -27,6 +27,8 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 export type RootTabParamList = {
   TabOne: undefined;
   TabTwo: undefined;
+  TabThree:undefined;
+  TabFour:undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
