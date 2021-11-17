187f83baca01521bde5a83f0e106f25ce9d59e3e
Justin Jiang <jiangj2620@gmail.com>
2021-11-17 18:54:59 -0500

    image upload

--- screens/PostScreen.tsx
+++ screens/PostScreen.tsx
@@ -1,26 +1,54 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Platform } from 'react-native';
 import PostHeader from '../components/PostHeader';
 import { IconButton, Colors } from 'react-native-paper';
-import ImagePicker from 'react-native-image-crop-picker'
+import * as ImagePicker from 'expo-image-picker';
+import { useEffect, useState } from 'react';
 
 
 export default function PostScreen() {
+    const [image, setImage] = useState(null);
 
+    useEffect(() => {
+        (async () => {
+          if (Platform.OS !== 'web') {
+            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            if (status !== 'granted') {
+              alert('Sorry, we need camera roll permissions to make this work!');
+            }
+          }
+        })();
+      }, []);
+
+      const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+        //   allowsEditing: true,
+        //   aspect: [5, 2],
+          quality: 1,
+        });
+    
+        console.log(result);
+    
+        if (!result.cancelled) {
+          setImage(result.uri);
+        }
+      };
+      
     return (
         <View>
             <PostHeader/>
             <View style={styles.container}>
-                <Text>Hello World</Text>
-            </View>
-            <IconButton
+                <IconButton
                 style={styles.addImageButton}
                 icon="plus"
                 color={Colors.black}
                 animated= {true}
                 size={30}
-                onPress={() => console.log('hello')}
+                onPress={pickImage}
             />
+            </View>
+            
 
         </View>
         
