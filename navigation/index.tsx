9af5992d2c13231f63794c376c22768138b8933a
Haichen Wang <hw595@cornell.edu>
2022-05-05 18:35:10 -0400

    Splash, app naming, theme

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -82,7 +82,7 @@ export default function Navigation({
   return (
     <NavigationContainer
       linking={LinkingConfiguration}
-      theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+      // theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
     >
       <RootNavigator onboard={onboard} />
     </NavigationContainer>
