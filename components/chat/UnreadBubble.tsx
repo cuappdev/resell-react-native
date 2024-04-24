d281de8f62dfc6b904500726357c35c18a99a55b
zachseidner1 <zachary.seidner@gmail.com>
2024-04-24 17:57:41 -0400

    Fix appearance of unread bubble

---
+++ components/chat/UnreadBubble.tsx
@@ -0,0 +1,22 @@
+import { Text, View } from "react-native";
+
+export default function UnreadBubble({
+  numberUnread,
+}: {
+  numberUnread: number;
+}) {
+  return (
+    <View
+      style={{
+        backgroundColor: "#FF0000",
+        borderRadius: 8,
+        width: 20,
+        marginLeft: 8,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ color: "#FFFFFF" }}>{numberUnread}</Text>
+    </View>
+  );
+}
